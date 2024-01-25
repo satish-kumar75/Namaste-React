a180b5f6d20d910ee003a130298ce46597b8550d
satsihkumar75 <Satishk404456@gmail.com>
2024-01-26 01:22:38 +0530

    Updating the data with live swiggy API

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,39 @@
-import resList from "../utils/mockData";
+import { useEffect, useState } from "react";
 import ResturantCard from "./ResturantCard";
 
 const Body = () => {
+  const [ListOfResturant, setListOfResturant] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=31.3260152&lng=75.57618289999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    setListOfResturant(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = ListOfResturant.filter(
+              (res) => res.info.avgRatingString > 4
+            );
+            setListOfResturant(filteredList);
+          }}
+        >
+          Filter
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((resturant) => (
+        {ListOfResturant.map((resturant) => (
           <ResturantCard key={resturant.info.id} resData={resturant} />
         ))}
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
-import logo from "../../assets/logo.svg";
+import logo from "../../assets/logo.png";
 
 const Header = () => {
   return (
     <div>
       <div className="header">
         <div className="logo-container">
-          <p>Bhukkhar</p>
+          {/* <p>Bhukkhar</p> */}
           <img src={logo} />
         </div>
         <div className="nav-item">
